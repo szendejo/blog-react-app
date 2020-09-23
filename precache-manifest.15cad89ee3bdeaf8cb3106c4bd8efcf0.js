cb53932c7fde9452bc86c4c2f7c0bfb5c53e9a01
Stephanie <szacademia@gmail.com>
2020-09-23 12:27:08 -0500

    Updates

---
+++ precache-manifest.15cad89ee3bdeaf8cb3106c4bd8efcf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f668260b54878f7665a062c62840a79",
+    "url": "/blog-react-app/index.html"
+  },
+  {
+    "revision": "de9b4c5118b123cb58bd",
+    "url": "/blog-react-app/static/css/main.1e18207e.chunk.css"
+  },
+  {
+    "revision": "ead26729fe346f4d02b8",
+    "url": "/blog-react-app/static/js/2.862c3f23.chunk.js"
+  },
+  {
+    "revision": "189e23ef3562d267680aa9086e84695d",
+    "url": "/blog-react-app/static/js/2.862c3f23.chunk.js.LICENSE"
+  },
+  {
+    "revision": "de9b4c5118b123cb58bd",
+    "url": "/blog-react-app/static/js/main.b450204f.chunk.js"
+  },
+  {
+    "revision": "86fd9355c55f01bd8f12",
+    "url": "/blog-react-app/static/js/runtime-main.1b351ef6.js"
+  }
+]);
